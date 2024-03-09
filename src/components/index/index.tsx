db6b8630f42b15c89ea60bd5a8bdae447a954c9d
koreacat <kj9966@naver.com>
2024-03-09 14:47:47 +0900

    feat: 리액트 개발자 도구 Suspense 예시 추가

--- src/components/index/index.tsx
+++ src/components/index/index.tsx
@@ -1,13 +1,17 @@
 import Templates from '../common/Templates'
 import Profile from './profile';
-import PlaceholderContents from './PlaceholderContents';
+import { Suspense, lazy } from 'react';
 import YoutubeIFrame from './YoutubeIFrame';
+
+const PlaceholderContents = lazy(() => import('./PlaceholderContents'))
 
 const Index = () => {
   return (
     <Templates>
       <Profile/>
-      <PlaceholderContents />
+      <Suspense fallback={'loading...'}>
+        <PlaceholderContents />
+      </Suspense>
       {/* <YoutubeIFrame /> */}
     </Templates>
   )
--- src/hooks/useJsonPlaceholder.ts
+++ src/hooks/useJsonPlaceholder.ts
@@ -4,6 +4,11 @@ import CommentRs from '../constants/CommentRs';
 function useJsonPlaceholder(): CommentRs | null {
   const [data, setData] = useState<CommentRs | null>(null);
 
+  /**
+   * TODO
+   * [hooks 기명 함수 사용하기]
+   */
+
   const fetchData = useCallback(async () => {
     const res = await fetch('https://jsonplaceholder.typicode.com/comments/1');
     const data = await res.json();
